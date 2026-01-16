85f197ee0990ae2dd9bff48249066a25e9b8e294
Eric Pietrowicz <pietrowicz.eric@gmail.com>
2026-01-16 09:27:00 -0700

    loading notification

--- app/designs/[owner]/[repo]/[sch-path]/loading.js
+++ app/designs/[owner]/[repo]/[sch-path]/loading.js
@@ -1,6 +1,12 @@
 export default function Loading () {
   return (
     <div className='w-full max-w-5xl mx-auto px-4'>
+      <div className='alert alert-info mt-6 mb-4'>
+        <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' className='stroke-current shrink-0 w-6 h-6'>
+          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+        </svg>
+        <span>This page is being indexed, this might take a few seconds.</span>
+      </div>
       <div className='flex items-start justify-between mt-6'>
         <div className='flex-1'>
           <div className='flex items-center space-x-2'>

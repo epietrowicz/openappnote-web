c332793057b61defc1ff56ab63490fa70f23fab4
Eric Pietrowicz <pietrowicz.eric@gmail.com>
2026-08-28 10:17:58 -0500

    clean up redirects and configs

--- next.config.mjs
+++ next.config.mjs
@@ -4,27 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'openappnote-bucket.nyc3.digitaloceanspaces.com'
-      },
-      {
-        protocol: 'https',
         hostname: 'avatars.githubusercontent.com'
-      }
-    ]
-  },
-  async redirects () {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'openappnote.dev' }],
-        destination: 'https://openappnote.com/:path*',
-        permanent: true
-      },
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.openappnote.com' }],
-        destination: 'https://openappnote.com/:path*',
-        permanent: true
       }
     ]
   }
